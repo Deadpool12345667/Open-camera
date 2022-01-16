9a9791e6e820c47ce353c1259468212f89d939a4
Deadpool12345667 <shuraimansari340@gmai.com>
2022-01-16 13:15:23 +0530

    initial commit

---
+++ app/src/main/java/com/example/opencamera/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.opencamera;
+
+import android.Manifest;
+import android.app.Notification;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    //initialize variable
+    ImageView imageView;
+    Button btOpen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Assign Variable
+        imageView = findViewById(R.id.image_view);
+        btOpen = findViewById(R.id.bt_open);
+
+        // Request For open camera permission
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    },
+                    100);
+
+        }
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open camera
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            ////Get Captured
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            // set capture image to imageview
+            imageView.setImageBitmap(captureImage);
+        }
+
+    }
+}
